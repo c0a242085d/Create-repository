e8ab70604c93a570a1e4644fcbf10309b643fda3
Tenta Hiratsuka <c0a242085d@edu.teu.ac.jp>
2025-06-10 17:16:33 +0900

    演習課題2終了

--- pygame_image.py
+++ pygame_image.py
@@ -22,19 +22,26 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
+        x=0
+        y=0
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(1, -1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(-1, 1)
-       # if key_lst[pg.K_LEFT]:
-            #kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
+            x=1
+            y=1
+        elif key_lst[pg.K_DOWN]:
+            x=-1
+            y=1
+        elif key_lst[pg.K_LEFT]:
+            x=1
+            y=0
+        elif key_lst[pg.K_RIGHT]:
+            x=1
+            y=0
         else:
-            kk_rct.move_ip(-1,0)
+            x=-1
+            y=0
         
-            
+        kk_rct.move_ip(x,y)    
             
 
         x = tmr%3200
